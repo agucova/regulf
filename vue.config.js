4ae9a3be1effff501b29aed2a9c77c66e750ce94
Agustín Covarrubias <agucova@uc.cl>
2021-04-23 00:11:31 -0400

    small fix

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,9 @@ module.exports = {
 
   publicPath: process.env.NODE_ENV === 'production' ? "/" : "/",
 
-    outputDir: "dist",
-    devServer: {
-      disableHostCheck: true
-    }
+  outputDir: "dist",
+  devServer: {
+    disableHostCheck: true
+  }
 
 }
